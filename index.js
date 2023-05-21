3f9233b5274572a916d880ca2d84bcc8ef844f9b
Olga L <113708045+OlgaLightning@users.noreply.github.com>
2023-05-21 17:44:33 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+//1
+let clinerSalary = 40000;
+let teacherSalary = 22000;
+console.log(`${clinerSalary + teacherSalary}`); //62000
+
+//2
+let title = 'Владивосток';
+let title2 = '2000';
+console.log(`${title} ${title2}`);
+
+//3
+let a = 128;
+let b = 254;
+let c = 137;
+let d = 201;
+let total = a + b + c + d;
+console.log(total); //720
+
+//4
+console.log(2**5); //32
+
+//5
+console.log('Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!');
+
+//6
+const greetings = "Привет";
+const name = "Кот";
+console.log(`${greetings}, ${name}`);
+
+//7
+let time = '';
+time = 34;
+console.log(`Старт поездки. Осталось минут: ${time}`);
+time -=15;
+console.log(`Немного сторис в соцсетях. Осталось минут: ${time}`)
+time -=10;
+console.log(`Немного не новостей, но событий. Осталось минут: ${time}`)
+time = 0;
+console.log('Вы доехали. Добро пожаловать в Москву')
+
+//8
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это по ${celsius} Цельсию.`);
